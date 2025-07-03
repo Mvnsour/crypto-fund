63ae4dff1a5df9c56a645b6541d300038dcd5d7e
Mvnsour <mbgmansour13@gmail.com>
2025-07-03 13:21:14 +0200

    feat: Implement basic CryptoFund contract structure

---
+++ contracts/CryptoFund.sol
@@ -0,0 +1,11 @@
+// Get funds from the user
+// Withdraw funds
+// Set a minimum funding value in USD
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.19 <0.9.0;
+
+contract CryptoFund {
+    
+}
