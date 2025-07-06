aae8fc15349d547b674a69e014e9d67029aa2316
Mvnsour <mbgmansour13@gmail.com>
2025-07-06 16:43:28 +0200

    Feat: Implement fund() function with minimum ETH requirement and comments

--- contracts/CryptoFund.sol
+++ contracts/CryptoFund.sol
@@ -2,13 +2,17 @@
 // Withdraw funds
 // Set a minimum funding value in USD
 
-
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.8.19 <0.9.0;
+pragma solidity ^0.8.19;
 
 contract CryptoFund {
-    function fund() public {
 
+    function fund() public payable {
+        // Allow user to send $
+        // Have a minimum $ sent
+        // To send ETH to this contract
+        require(msg.value >= 1e18, "didn't send enough ETH"); //1e18 = 1 ETH = 1000000000000000000 = 1 * 10 **(18)
+        //if the condition is false, revert with the error message
     }
 
     function withdraw() public {
