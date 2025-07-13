1005c2c1835666c92dfd1631bfa512f11ef85da1
Mvnsour <mbgmansour13@gmail.com>
2025-07-13 15:04:34 +0200

    feat: track number of fund calls per user and expose it via contributionCount()

--- contracts/CryptoFund.sol
+++ contracts/CryptoFund.sol
@@ -15,6 +15,11 @@ contract CryptoFund {
 
     // Mapping to track how much each funder has contributed (address => amount)
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+    mapping(address funder => uint256 amountContributed) public contributionCounter;
+
+    function contributionCount(address funder)public view returns(uint256) {
+        return contributionCounter[funder];
+    }
 
     function fund() public payable {
         // Allow user to send $
@@ -25,7 +30,9 @@ contract CryptoFund {
         // Add sender to funders array if not already present
         funders.push(msg.sender);
         // Update the amount funded by this sender
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender] + msg.value;
+        addressToAmountFunded[msg.sender] += msg.value;
+        // Increment the contribution counter for this sender
+        ++contributionCounter[msg.sender];
     }
 
     function withdraw() public {
@@ -53,10 +60,10 @@ contract CryptoFund {
     function getConversionRate(uint256 ethAmount) public view returns(uint256) {
         // How much 1 ETH worth?
         // 2935_29000000000000000
-        uint256 ethPrice = getPrice();
+        uint256 ethPrice = getPrice(); // ETH/USD with 18 decimals
         // (2935_29000000000000000 * 1_000000000000000000) / 1000000000000000000
-        // $2935.2 = 1 ETH
-        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18;
+        // $2935.29 = 1 ETH
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; // ETH to USD
         return ethAmountInUsd;
     }
 
@@ -64,4 +71,10 @@ contract CryptoFund {
         return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306).version();
     }
 
+
+    /* if we want the opposite (USD in ETH) we can do that
+    function convertUsdToEth(uint256 usdAmount, uint256 ethPrice) public pure returns(uint256) {
+        uint256 ethAmount = (usdAmount * 1e18) / ethPrice;
+        return ethAmount;
+    } */
 }
